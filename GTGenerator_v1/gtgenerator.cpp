8622e07be0d0a4b44e7755449585df3fe5ea9081
Santiago <santiago.molina.g@gmail.com>
2015-12-16 14:04:23 -0500

    Added some signals for new project dialog

--- GTGenerator_v1/gtgenerator.cpp
+++ GTGenerator_v1/gtgenerator.cpp
@@ -3,10 +3,57 @@
 GTGenerator::GTGenerator(QWidget *parent)
 	: QMainWindow(parent)
 {
-	ui.setupUi(this);
+	//Create Gui variable
+	GTgui = new Ui_GTGeneratorClass();
+	GTgui->setupUi(this);
+
+	//------------------------------------------------------------------------------------
+	//New project slots and initialization
+
+	//Create NewProject Dialog
+	newProject = new QDialog(this);
+	ui_newProject = new Ui_newProject_dialog();
+	ui_newProject->setupUi(newProject);
+
+	connect(GTgui->actionNewProject, SIGNAL(triggered()), this, SLOT(showNewProjectDialog()));
+
+	connect(ui_newProject->sourceVideo_button, SIGNAL(stateChanged()), this, SLOT(selectVideo()));
+
+	//------------------------------------------------------------------------------------
 }
 
 GTGenerator::~GTGenerator()
 {
 
+}
+
+void GTGenerator::showNewProjectDialog()
+{
+	//Show new project dialog
+	newProject->exec();
+}
+
+void GTGenerator::selectVideo()
+{
+
+}
+
+void GTGenerator::selectImageSeq()
+{
+
+}
+
+void GTGenerator::loadSource()
+{
+	//Load video dialog
+	if (flag_src == 1)
+	{
+
+	}
+
+	//Load image sequence dialog
+	if (flag_src == 2)
+	{
+
+	}
 }
--- GTGenerator_v1/gtgenerator.h
+++ GTGenerator_v1/gtgenerator.h
@@ -3,6 +3,9 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_gtgenerator.h"
+#include "ui_newProject.h"
+#include "ui_saveProject.h"
+#include <QtMultimediaWidgets>
 
 class GTGenerator : public QMainWindow
 {
@@ -13,7 +16,34 @@ public:
 	~GTGenerator();
 
 private:
+
+	//Variables---------------
+
+	//Main GT Gui
 	Ui::GTGeneratorClass ui;
+	Ui_GTGeneratorClass * GTgui;
+
+	//New project Gui
+	QDialog *newProject;
+	Ui_newProject_dialog *ui_newProject;
+
+
+	int flag_src;	//Data source flag (1: video, 2: image sequence)
+
+public slots:
+
+//New project dialog slots
+
+void showNewProjectDialog();
+
+void selectVideo();
+
+void selectImageSeq();
+
+void loadSource();
+
+
+
 };
 
 #endif // GTGENERATOR_H
